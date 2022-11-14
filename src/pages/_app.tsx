87f6085f8c9bb5700f78138b38e8e78ffa006fc0
Katsushi OUGI <ougi@to-r.net>
2022-11-14 16:34:01 +0900

    auth0の設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../../styles/globals.css'
+// import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps } from 'next/app'
+
+import '../../styles/globals.css'
 
 require('../mocks')
 
