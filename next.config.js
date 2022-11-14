7f7432bc83a0f0bea360df70f9384a10d2d4560d
Katsushi OUGI <ougi@to-r.net>
2022-11-14 18:51:51 +0900

    msw対応

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // TODO: mswでエラーが出るので一時的な対応
+  experimental: {
+    esmExternals: false,
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect } from 'react'
 import axios from 'axios'
+import useSWR from "swr";
 
 import styles from '../../styles/Home.module.css'
 import { API_URL } from '../config'
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(`${API_URL}/users`)
-      console.log('response', response)
-    })()
-  }, [])
+  const { data, error } = useSWR(`${API_URL}/users`, async (url) => {
+    const response = await axios.get(url)
+
+    return response
+  })
+
+  console.log('data', data)
 
   return (
     <div className={styles.container}>
@@ -20,3 +20,4 @@ export default function Home() {
     </div>
   )
 }
+
